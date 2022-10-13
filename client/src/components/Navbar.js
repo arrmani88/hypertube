de232ce048c3bcee4228bffe195a4886c61a650d
Anas El boudali <anel-bou@student.1337.ma>
2022-10-13 23:53:24 +0100

    avatar drawer --in progress

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,25 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/Navbar.css'
+import Drawer from '@mui/material/Drawer';
 import { BiMenuAltRight } from 'react-icons/bi'
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
+
+
+const SectionDrawer = (props) => {
+  return (
+	<div className='sectionDrawer'>					
+		<MdChevronLeft className='sectionIcon'/>
+		<h1>{props.title}</h1>
+		<div className='sectionIcon'/>
+	</div>
+  )
+}
+
 
 const Navbar = () => {
-  return (
-    <div className='navbarContainer'>
 
-      <div className='middleSections'>
-        <div className='balanceDivAvatar' />
-        <h1 className='section'>About</h1>
-        <h1 className='section'>Trailer</h1>
-        <img onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})} className='logo' src='images/hypertube_logo.png' alt={'logo'} />
-        <h1 className='section'>Actors</h1>
-        <h1 className='section'>Settings</h1>
-        <div className='balanceDivAvatar' />
-      </div>
-      
-      <img className='avatarUser' src='images/arrmani88.jpeg' />
-      <BiMenuAltRight className='menuIcon'></BiMenuAltRight>
-    </div>
-  )
+	const [drawerState, setDrawerState] = useState(true)
+
+	const changeDrawerState = () => setDrawerState(!drawerState)
+
+	return (
+		<div className='navbarContainer'>
+
+			<div className='balanceEmptyDivAvatar' />
+			<div className='navbarSections'>
+				<h1 className='sectionNavbar'>About</h1>
+				<div className='sectionSpaceEmptyDiv' />
+				<h1 className='sectionNavbar'>Trailer</h1>
+				<img onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} className='logo' src='images/hypertube_logo.png' alt={'logo'} />
+				<h1 className='sectionNavbar'>Actors</h1>
+				<h1 className='sectionNavbar'>Settings</h1>
+			</div>
+
+			<img className='avatarUserNavbar' src='images/arrmani88.jpeg' alt='userImg' />
+			<BiMenuAltRight onClick={changeDrawerState} className='menuIcon'></BiMenuAltRight>
+			<Drawer
+				anchor='right'
+				open={drawerState}
+				variant='temporary'
+				sx={{
+					width: 250,
+					flexShrink: 0,
+          			'& .MuiDrawer-paper': {
+            			width: 250,
+					}
+				}}
+			>
+				<div>
+					<div className='drawerHeader'>
+						<div className='drawerHeaderGradient' />
+						<img className='avatarUserDrawer' src='images/arrmani88.jpeg' alt='userImg' />
+						<h1 className='userFullName'>Anas EL BOUDALI</h1>
+						<MdChevronRight onClick={changeDrawerState} className='mdChevronRight'/>
+					</div>
+				</div>
+				<div>
+					<SectionDrawer title='About' />
+					<SectionDrawer title='Trailer' />
+					<SectionDrawer title='About' />
+					<SectionDrawer title='Settings' />
+				</div>
+
+			</Drawer>
+		</div>
+	)
 }
 
 // const Navbar = () => {
@@ -35,4 +82,5 @@ const Navbar = () => {
 // }
 
 export default Navbar
-// /goinfre/anel-bou/projects/hypertube/client/public/images/arrmani88.jpeg
+
+
