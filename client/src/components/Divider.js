0fe5df4095ee6a4cb56f9054bdbb5a0c8c0a964d
Anas El boudali <anel-bou@student.1337.ma>
2022-10-21 19:52:11 +0100

    ma3reftch chno nzid

--- client/src/components/Divider.js
+++ client/src/components/Divider.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import './styles/Divider.css'
+
+const line = {
+	width: "100%",
+	borderBottom: "1px solid lightgray"
+}
+
+const container = {
+	display: "flex",
+	alignItems: "center",
+	flexDirection: "row",
+	padding: "20px"
+}
+
+const content = {
+	margin: "0 10px 0 10px",
+	color: "white",
+	fontSize: "25px"
+}
 
 const Divider = ({children}) => {
   return (
-	<div className='container'>
-
+	<div style={container}>
+		<div style={line}/>
+		<div style={content}>{children}</div>
+		<div style={line}/>
 	</div>
   )
 }
 
 export default Divider
+
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './styles/Landing.css'
-import { BsPlayFill } from 'react-icons/bs'
+import { BsFacebook, BsPlayFill } from 'react-icons/bs'
 import { NavbarUserUnlogged } from '../components/Navbar'
 import Divider from '../components/Divider'
+import { ImGoogle3 } from 'react-icons/im'
+import { useRef, useState } from 'react'
 
 const Landing = () => {
+	const emailRef = useRef(null)
+	const [isEmailValid, setIsEmailValid] = useState(true)
+
+	const click = () => {
+		setIsEmailValid(Boolean(String(emailRef.current.value).toLowerCase()
+			.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+		))
+	}
 	return (
 	<>
 		<NavbarUserUnlogged />
@@ -14,16 +24,27 @@ const Landing = () => {
 			<div className='card'>
 				<h1 className='cardTitle'>See what's next!</h1>
 				<h1 className='cardText1'>Watch limitless anytime, anywhere...</h1>
-				<h1 className='cardText2'>Ready to watch? Enter your email to create or restart your membership.</h1>
+				<h1 className='cardText2 hideOnMobileVersion'>Ready to watch? Enter your email to create or restart your membership.</h1>
 				<div>
-					<input className='emailField' type='text' />
-					<button className='cardButton'>
+				{/* `emailField (${isEmailValid === false && 'emailFielError'})` */}
+					<input className={`emailField ${isEmailValid === false && 'emailFieldError'}`} ref={emailRef} onChange={() => setIsEmailValid(true)} placeholder='Enter your Email' type='text' />
+					<div className='containerInvalidEmail'>
+						{isEmailValid === false
+							? <h1 className='invalidEmail'>Invalid email address</h1>
+							: <h1 className='invalidEmail'> </h1>
+						}
+					</div>
+					<button className='cardButton' onClick={ click }>
 						<h1 className='buttonText'>START WATCHING FOR FREE</h1>
 						<BsPlayFill className='cardPlayIcon' />
 					</button>
-					<Divider>
-						<h1>-----------------------------</h1>
-					</Divider>
+					<Divider><h1>Or</h1></Divider>
+					<h1 className='cardText2'>Continue with:</h1>
+					<div className='socialsContainer'>
+						<BsFacebook className='socialMediaIcon' />
+						<ImGoogle3 className='socialMediaIcon' />
+						<img src='images/42_icon.png' className='icon42' alt='42_icon' />
+					</div>
 				</div>
 			</div>
 		</div>
