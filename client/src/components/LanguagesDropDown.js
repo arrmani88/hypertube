8edd380ae4ab775839fbb49ed3d377a4750bf1bd
Anas El boudali <anel-bou@e2r6p14.1337.ma>
2022-10-24 21:30:44 +0100

    setting dropdown buddan

--- client/src/components/LanguagesDropDown.js
+++ client/src/components/LanguagesDropDown.js
@@ -1,12 +1,23 @@
-import {useSelect} from 'react-aria'
-
+import { useSelect } from 'react-aria'
+import { US, ES, DE } from 'country-flag-icons/react/3x2'
 import React from 'react'
+import './styles/LanguagesDropDown.css'
+import { useState } from 'react'
 
 const LanguagesDropDown = () => {
-  return (
-	
-  )
+	const [menuState, setMenuState] = useState(false)
+
+	return (
+		<div className='languagesDropDown' onClick={ () => setMenuState(!menuState) }>
+			<US className='flag' />
+			{menuState === true
+				? <div className='dropdownMenu' >
+				</div>
+				: <div/>
+			}
+
+		</div>
+	)
 }
 
 export default LanguagesDropDown
-
