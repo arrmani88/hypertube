7865e02f9427d9cb876e4470c7444c441f98b49f
arrmani88 <anel-bou@student.1337.ma>
2022-10-30 07:39:25 +0100

    setting gender selection to be validated

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavbarUserUnlogged } from '../components/Navbar'
 import styles from './styles/Register.module.css'
 import * as yup from 'yup'
@@ -14,11 +14,13 @@ const userSchema = yup.object({
 	lastName: 		yup.string().required('Required field').matches(regex.kFirstName, 'Invalid last name'),
 	birthday: 		yup.string().required('Required field'),
 	email: 			yup.string().required('Required field').email('Invalid email address'),
+	gender:			yup.string().required('Required field'),
 	password: 		yup.string().required('Required field').min(6, passwordLengthMessage).max(20, passwordLengthMessage),
 	confirmPassword: yup.string().required('Required field').oneOf([yup.ref('password')], "Passwords should match")
 }).required()
 
 const Register = () => {
+	const [gender, setGender] = useState('')
 	const { t } = useTranslation()
 	const { register, handleSubmit, formState: { errors } } = useForm({
 		resolver: yupResolver(userSchema)
@@ -46,18 +48,22 @@ const Register = () => {
 							<h1>{errors.lastName?.message ? errors.lastName?.message : ' '} </h1>
 						</label>
 						<label>
-							<p className={styles.label}>{t('e_mail')}-mail</p>
+							<p className={styles.label}>{t('e_mail')}</p>
 							<input {...register('email')} placeholder='Your E-mail' />
 							<h1>{errors.email?.message || ' '}</h1>
 						</label>
 						<label>
 							<p className={styles.label}>{t('birthday')}</p>
-							<input {...register('birthday')} placeholder='Your birthday' />
+							<input {...register('birthday')} type='date' />
 							<h1>{errors.birthday?.message || ' '}</h1>
 						</label>
 						<label>
-							<p className={styles.label}>{t('gender')}</p>
-							<input {...register('gender')} placeholder='Your gender' />
+							<div className={styles.avatarsContainer} >
+								<p className={styles.label}>{t('gender')}</p>
+								<input {...register('gender')} type='hidden' />
+								<img onClick={() => {setGender('female'); setValue('gender', gender)}} className={styles.avatar + ' ' + (gender === 'female' && styles.selectedAvatar)} src='images/woman-avatar.svg' alt='man-avatar' />
+								<img onClick={() => {setGender('male'); setValue('gender', gender)}} className={styles.avatar + ' ' + (gender === 'male' && styles.selectedAvatar)} src='images/man-avatar.svg' alt='man-avatar' />
+							</div>
 							<h1>{errors.gender?.message || ' '}</h1>
 						</label>
 						<label>
@@ -70,8 +76,8 @@ const Register = () => {
 							<input {...register('confirmPassword')} placeholder='Re-type your password' />
 							<h1>{errors.confirmPassword?.message || ' '}</h1>
 						</label>
-						<button type='submit' className={styles.submitButton}>
-							<p>Register</p>
+						<button type='submit' className={styles.submitButton} onClick={console.log('gender=' + gender)}>
+							<p>{t('register')}</p>
 							<BsPlayFill className={`text-[40px]`} />
 						</button>
 					</form>
