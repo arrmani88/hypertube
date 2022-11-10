2ddac6497f7ebec38290b8fed5dc4ac5be0590b2
arrmani88 <anel-bou@student.1337.ma>
2022-11-10 17:05:52 +0100

    storing accessToken in login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -10,18 +10,41 @@ import IMGnationalTreasure from '../images/national-treasure.jpg'
 import IMG42icon from '../images/42_icon.png'
 import axios from 'axios'
 import Loading from '../components/Loading'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
 
 const Login = () => {
 	const [pageState, setPageState] = useState('loading') // should be either loading or loading
 	const { t } = useTranslation()
 	const navigate = useNavigate()
-	const login = (e) => {
-		e.preventDefault()
-		/* log usr in */
+	const loginSchema = yup.object({
+		login: yup.string().required(t('required_field')),
+		password: yup.string().required(t('required_field')).min(6, 'Invalid password').max(20, 'Invalid password'),
+	}).required()
+	const { register, handleSubmit, formState: { errors } } = useForm({
+		resolver: yupResolver(loginSchema)
+	})
+
+	const submitForm = async (loginData) => {
+		try {
+			console.log(loginData)
+			const rsp = await axios.post(
+				`${process.env.REACT_APP_SERVER_HOSTNAME}/login`,
+				loginData,
+				// {headers: {
+				// 	'Content-Type': 'application/json'
+				// }}
+			)
+			console.log(rsp)
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
 	useEffect(() => {
-		const checkAlreadyLogged = async (navigate) => {
+		const checkAlreadyLogged = async () => {
 			try {
 				const storedAccessToken = localStorage.getItem('accessToken')
 				if (!storedAccessToken) navigate('/login') // if no token was stored, continue to the page
@@ -34,47 +57,42 @@ const Login = () => {
 				}
 			} catch (err) {
 				console.log(err)
-				if (err.response && err.response.status / 100 === 4) setPageState('login')  // err.rsp.status / 100 -> means 400 or 401 or 403
+				if (err.response && err.response.status / 100 === 4) setPageState('login') // err.rsp.status / 100 -> means 400 or 401 or 403
 			}
 		}
 		checkAlreadyLogged()
 	}, [])
 
 	return (<>
-		{(() => {
-			switch (pageState) {
-				case 'loading':
-					return <Loading />
-				default:
-					return <CardThemeBackground imgLink={IMGnationalTreasure} loginButtonHidden={true} >
-						<p className={styles.cardTitle} >{t('login_and_start_watching')}</p>
-						<form className={styles.loginForm} >
-							<label >
-								<p>Login</p>
-								<input className={styles.field} />
-							</label>
-							<div className='mt-[10px]' />
-							<label >
-								<p>{t('password')}</p>
-								<input className={styles.field} />
-							</label>
-							<div className='mt-[20px]' />
-							<button className={styles.loginButton} onClick={login} >
-								<h1 className={styles.loginText}>{t('login')}</h1>
-								<BsPlayFill className={`text-[40px]`} />
-							</button>
-						</form>
-						<Divider><h1>{t('or')}</h1></Divider>
-						<h1 className={styles.continueWith}>{t('continue_with')}</h1>
-						<div className={styles.socialsContainer} >
-							<BsFacebook className={styles.socialMediaIcon} />
-							<ImGoogle3 className={styles.socialMediaIcon} />
-							<img src={IMG42icon} className={styles.icon42} alt='42_icon' />
-						</div>
-					</CardThemeBackground>
-			}
-		})()}
-
+		{pageState === 'loading'
+			? <Loading />
+			: <CardThemeBackground imgLink={IMGnationalTreasure} loginButtonHidden={true} >
+				<p className={styles.cardTitle} >{t('login_and_start_watching')}</p>
+				<form onSubmit={handleSubmit(submitForm)} className={styles.loginForm} >
+					<label >
+						<p>Login</p>
+						<input {...register('login')} className={styles.field} />
+					</label>
+					<div className='mt-[10px]' />
+					<label >
+						<p>{t('password')}</p>
+						<input {...register('password')} className={styles.field} />
+					</label>
+					<div className='mt-[20px]' />
+					<button type='submit' className={styles.loginButton} >
+						<h1 className={styles.loginText}>{t('login')}</h1>
+						<BsPlayFill className={`text-[40px]`} />
+					</button>
+				</form>
+				<Divider><h1>{t('or')}</h1></Divider>
+				<h1 className={styles.continueWith}>{t('continue_with')}</h1>
+				<div className={styles.socialsContainer} >
+					<BsFacebook className={styles.socialMediaIcon} />
+					<ImGoogle3 className={styles.socialMediaIcon} />
+					<img src={IMG42icon} className={styles.icon42} alt='42_icon' />
+				</div>
+			</CardThemeBackground>
+		}
 	</>)
 }
 
