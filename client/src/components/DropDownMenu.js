f30ce2e14008999c8d4207adb83be565e143caf0
arrmani88 <anel-bou@student.1337.ma>
2022-12-15 22:57:08 +0100

    setting :3000/search

---
+++ client/src/components/DropDownMenu.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState, useRef, useEffect } from 'react'
+import styles from './styles/DropDownMenu.module.css'
+import { IoCloseCircle } from 'react-icons/io5'
+
+const DropDownMenu = ({ childs, controller, keyName, className }) => {
+	const [menuState, setMenuState] = useState(false)
+	const dropdownRef = useRef(null)
+
+	const changeGenre = (selectedItem) => {
+		controller.setQueryParams({
+			...controller.queryParams,
+			 [keyName]: selectedItem
+		})
+	}
+console.log(controller)
+
+	useEffect(() => {
+		let handleClick = (e) => {
+			if (dropdownRef.current && !(dropdownRef.current.contains(e.target)))
+				setMenuState(false)
+		}
+		document.addEventListener('mousedown', handleClick)
+		return () => document.removeEventListener('mousedown', handleClick)
+	})
+
+	return (
+		<div className={styles.container +' '+ className} onClick={() => setMenuState(!menuState)} ref={dropdownRef} >
+			<div className={styles.dropdownButton} >
+				<h1 className={controller.queryParams[keyName] ? '' : 'font-bold'}>
+					{controller.queryParams[keyName] ?? keyName }
+				</h1>
+			</div>
+
+			<div className={styles.dropdownMenu +' '+ (menuState ? styles.shown : styles.hidden)} >
+
+				<div className='flex row items-center font-bold mb-[8px] cursor-pointer' >
+					<div className={styles.item} onClick={() => { changeGenre(null) }} >
+						Deselect
+					</div>
+					<IoCloseCircle className='pb-[2px]' />
+				</div>
+
+				{childs.map((item) => (
+					<div className='w-full mt-[3px]' key={item}>
+						<div className={styles.item} onClick={() => { changeGenre(item) }} >
+							{item}
+						</div>
+						{childs[childs.length - 1] !== item && <div className={styles.divider} />}
+					</div>
+				))}
+
+			</div>
+		</div>
+	)
+}
+
+export default DropDownMenu
+
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,11 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import CardThemeBackground from '../components/CardThemeBackground'
 import { hideLoading } from '../redux/loadingSlice'
 import IMGpeakyBlinders from '../images/peaky_blinders.jpg'
+import styles from './styles/Search.module.css'
+import { BsSearch } from 'react-icons/bs'
+import DropDownMenu from '../components/DropDownMenu'
+
+const genres = ['Comedy', 'Sci-fi', 'Horror', 'Romance', 'Action', 'Thriller', 'Drama', 'Mystery', 'Crime', 'Animation', 'Adventure', 'Fantasy']
+const qualities = ['720p', '1080p', '2160p', '3D']
+// limit - pageNumber - quality - minRating/10 - sortBy - orderBy 
+// genre 
 
 const Search = () => {
 	const dispatch = useDispatch()
+	const [queryParams, setQueryParams] = useState({})
 
 	useEffect(() => {
 		dispatch(hideLoading())
@@ -13,7 +22,20 @@ const Search = () => {
 
 	return (
 		<CardThemeBackground imgLink={IMGpeakyBlinders} >
-			<div className='h-[500px] w-[500px]' ></div>
+			<div className={styles.container} >
+				<div className='row flex ' >
+					<input className={styles.searchField} placeholder='Search' />
+					<DropDownMenu childs={genres} keyName='Genre' controller={{queryParams, setQueryParams}} className='ml-[10px]' />
+					<DropDownMenu childs={qualities} keyName='Quality' controller={{queryParams, setQueryParams}} className='ml-[10px]' />
+					<button className={styles.searchButton} >
+						<BsSearch />
+					</button>
+				</div>
+				{/* <input className={styles.searchField} placeholder='Search' /> */}
+				<div className='mt-[20px]' />
+				<div className='flex row' >
+				</div>
+			</div>
 		</CardThemeBackground>
 	)
 }
